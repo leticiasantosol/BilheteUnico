cf2e28f75102777ced1a9ad6fe2f835652327d66
Leticia Santos <leticinhadeoliveira19@gmail.com>
2026-08-07 17:15:18 -0300

    feat: adiciona atributos

--- src/main/java/BilheteUnico/BilheteComum.java
+++ src/main/java/BilheteUnico/BilheteComum.java
@@ -1,5 +1,10 @@
 package BilheteUnico;
 
 public class BilheteComum extends BilheteUnico {
+    public BilheteComum (String codigoDoBilhete, Usuario usuarioInformacoes) {
+        super(codigoDoBilhete, usuarioInformacoes);
+    }
 
+    private double saldo;
+    private static final double valorPassagem = 5.0;
 }
