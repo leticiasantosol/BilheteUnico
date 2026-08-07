44952b2f5eb02e43a3ce7abd56ec48f506bd8c60
Leticia Santos <leticinhadeoliveira19@gmail.com>
2026-08-06 22:34:51 -0300

    feat: adiciona construtores

--- src/main/java/BilheteUnico/Usuario.java
+++ src/main/java/BilheteUnico/Usuario.java
@@ -1,4 +1,12 @@
 package BilheteUnico;
 
 public class Usuario {
+    private String nome, email, cpf, telefone;
+
+    public Usuario (String nomeDaPessoa,String emailDaPessoa, String cpfDaPessoa, String telefoneDaPessoa) {
+        this.nome = nomeDaPessoa;
+        this.email = emailDaPessoa;
+        this.cpf = cpfDaPessoa;
+        this.telefone = telefoneDaPessoa;
+    }
 }
