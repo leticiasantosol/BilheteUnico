1c5395b3d500c75e8dfd64c58b0050c73f9de845
Leticia Santos <leticinhadeoliveira19@gmail.com>
2026-08-06 22:36:46 -0300

    feat: adiciona getters e setters

--- src/main/java/BilheteUnico/Usuario.java
+++ src/main/java/BilheteUnico/Usuario.java
@@ -9,4 +9,32 @@ public class Usuario {
         this.cpf = cpfDaPessoa;
         this.telefone = telefoneDaPessoa;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
