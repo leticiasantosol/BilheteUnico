5eddc1519db121605c1f7940fc117389f8c44f8f
Leticia Santos <leticinhadeoliveira19@gmail.com>
2026-08-06 22:44:15 -0300

    adiciona getters e setters BilheteUnico

--- src/main/java/BilheteUnico/BilheteUnico.java
+++ src/main/java/BilheteUnico/BilheteUnico.java
@@ -1,4 +1,22 @@
 package BilheteUnico;
 
 public class BilheteUnico {
+    private String codigo;
+    private Usuario usuario;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
