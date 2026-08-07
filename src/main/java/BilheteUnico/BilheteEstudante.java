857f8ef515e8202debe84248a5ab24f69fd3da1d
Leticia Santos <leticinhadeoliveira19@gmail.com>
2026-08-07 17:24:44 -0300

    feat: adiciona atributos

--- src/main/java/BilheteUnico/BilheteEstudante.java
+++ src/main/java/BilheteUnico/BilheteEstudante.java
@@ -1,5 +1,9 @@
 package BilheteUnico;
 
 public class BilheteEstudante extends BilheteUnico{
+    public BilheteEstudante (String codigoDoBilhete, Usuario usuarioInformacoes) {
+        super(codigoDoBilhete, usuarioInformacoes);
+    }
 
+    private int cotas;
 }
